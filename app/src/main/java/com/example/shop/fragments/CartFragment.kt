3409dd8f553a89251fb84b803e0ba94fe31fa700
Kirill Tremaskin <tremaskinld@yandex.ru>
2022-01-15 01:35:37 +0300

    lab 12, added flow

--- app/src/main/java/com/example/shop/fragments/CartFragment.kt
+++ app/src/main/java/com/example/shop/fragments/CartFragment.kt
@@ -1,18 +1,19 @@
 package com.example.shop.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
-import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.shop.R
 import com.example.shop.activity.MainActivity
 import com.example.shop.adapter.CartAdapter
 import com.example.shop.databinding.FragmentCartBinding
+import com.example.shop.ext.onClickFlow
+import com.example.shop.ext.onRefreshFlow
+import com.example.shop.model.Product
 import com.example.shop.network.NetworkService
-import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 class CartFragment: Fragment(R.layout.fragment_cart) {
 
@@ -22,10 +23,6 @@ class CartFragment: Fragment(R.layout.fragment_cart) {
 
     private lateinit var binding: FragmentCartBinding
 
-    private val scope = CoroutineScope(Dispatchers.Main + Job() + CoroutineExceptionHandler { _, exception ->
-        showError(exception)
-    })
-
     private val adapter = CartAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,29 +30,58 @@ class CartFragment: Fragment(R.layout.fragment_cart) {
         binding = FragmentCartBinding.bind(view)
         (activity as? MainActivity)?.changeMenuState(isVisible = true)
 
-        with(binding) {
-            recyclerViewCart.adapter = adapter
-            swipeRefreshLayout.setOnRefreshListener {
-                getCart()
-            }
+        binding.recyclerViewCart.adapter = adapter
+
+        merge(
+            flowOf(Unit),
+            binding.swipeRefreshLayout.onRefreshFlow(),
+            binding.textViewUpdate.onClickFlow()
+        ).flatMapLatest {
+            getCart()
         }
-
-        getCart()
+            .distinctUntilChanged()
+            .onEach {
+                when (it) {
+                    is ScreenState.DataLoaded -> {
+                        setStateLoading(false)
+                        setStateError(null)
+                        setStateData(it.products)
+                    }
+                    is ScreenState.Error -> {
+                        setStateLoading(false)
+                        setStateError(it.error)
+                        setStateData(null)
+                    }
+                    ScreenState.Loading -> {
+                        setStateLoading(true)
+                        setStateError(null)
+                        setStateData(null)
+                    }
+                }
+            }.launchIn(lifecycleScope)
     }
 
-    private fun showError(error: Throwable) {
-        Toast.makeText(context, getString(R.string.error, error.toString()), Toast.LENGTH_LONG).show()
-        binding.progressBar.isGone = true
+    private fun getCart() = flow {
+        emit(ScreenState.Loading)
+        val catalogProducts = NetworkService.loadCart()
+        emit(ScreenState.DataLoaded(catalogProducts))
+    }.catch {
+        emit(ScreenState.Error(getString(R.string.just_error)))
     }
 
-    private fun getCart() {
-        scope.launch {
-            adapter.clearCart()
-            binding.progressBar.isVisible = true
-            val cartProducts = NetworkService.loadCart()
-            adapter.setProducts(cartProducts)
-            binding.progressBar.isGone = true
-            binding.swipeRefreshLayout.isRefreshing = false
-        }
+    private fun setStateLoading(isLoading: Boolean) = with(binding) {
+        progressBar.isVisible = isLoading && !recyclerViewCart.isVisible
+        swipeRefreshLayout.isRefreshing = isLoading && recyclerViewCart.isVisible
     }
+
+    private fun setStateData(products: List<Product>?) = with(binding) {
+        recyclerViewCart.isVisible = products != null
+        adapter.setProducts(products ?: emptyList())
+    }
+
+    private fun setStateError(error: String?) = with(binding) {
+        constraintLayoutError.isVisible = error != null
+        textViewError.text = error
+    }
+
 }
--- app/src/main/java/com/example/shop/fragments/CatalogFragment.kt
+++ app/src/main/java/com/example/shop/fragments/CatalogFragment.kt
@@ -6,14 +6,19 @@ import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.shop.R
 import com.example.shop.activity.MainActivity
 import com.example.shop.adapter.CatalogAdapter
 import com.example.shop.data.DataSource
 import com.example.shop.databinding.FragmentCatalogBinding
+import com.example.shop.ext.onClickFlow
+import com.example.shop.ext.onRefreshFlow
+import com.example.shop.model.Product
 import com.example.shop.network.NetworkService
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 class CatalogFragment : Fragment(R.layout.fragment_catalog) {
 
@@ -22,10 +27,6 @@ class CatalogFragment : Fragment(R.layout.fragment_catalog) {
     }
 
     private lateinit var binding: FragmentCatalogBinding
-
-    private val scope = CoroutineScope(Dispatchers.Main + Job() + CoroutineExceptionHandler { _, exception ->
-        showError(exception)
-    })
 
     private val adapter = CatalogAdapter()
 
@@ -37,27 +38,64 @@ class CatalogFragment : Fragment(R.layout.fragment_catalog) {
         with(binding) {
             recyclerViewProducts.layoutManager = GridLayoutManager(requireContext(), 2)
             recyclerViewProducts.adapter = adapter
-            swipeRefreshLayout.setOnRefreshListener {
-                getProducts()
-            }
+
         }
 
-        getProducts()
-    }
-
-    private fun showError(error: Throwable) {
-        Toast.makeText(context, getString(R.string.error, error.toString()), Toast.LENGTH_LONG).show()
-        binding.progressBar.isGone = true
-    }
-
-    private fun getProducts() {
-        scope.launch {
-            adapter.clearProducts()
-            binding.progressBar.isVisible = true
-            val catalogProducts = NetworkService.loadCatalog()
-            adapter.setProducts(catalogProducts)
-            binding.progressBar.isGone = true
-            binding.swipeRefreshLayout.isRefreshing = false
+        merge(
+            flowOf(Unit),
+            binding.swipeRefreshLayout.onRefreshFlow(),
+            binding.textViewUpdate.onClickFlow()
+        ).flatMapLatest {
+            getProducts()
         }
+            .distinctUntilChanged()
+            .onEach {
+                when (it) {
+                    is ScreenState.DataLoaded -> {
+                        setStateLoading(false)
+                        setStateError(null)
+                        setStateData(it.products)
+                    }
+                    is ScreenState.Error -> {
+                        setStateLoading(false)
+                        setStateError(it.error)
+                        setStateData(null)
+                    }
+                    ScreenState.Loading -> {
+                        setStateLoading(true)
+                        setStateError(null)
+                        setStateData(null)
+                    }
+                }
+            }.launchIn(lifecycleScope)
     }
+
+    private fun getProducts() = flow {
+        emit(ScreenState.Loading)
+        val catalogProducts = NetworkService.loadCatalog()
+        emit(ScreenState.DataLoaded(catalogProducts))
+    }.catch {
+        emit(ScreenState.Error(getString(R.string.just_error)))
+    }
+
+    private fun setStateLoading(isLoading: Boolean) = with(binding) {
+        progressBar.isVisible = isLoading && !recyclerViewProducts.isVisible
+        swipeRefreshLayout.isRefreshing = isLoading && recyclerViewProducts.isVisible
+    }
+
+    private fun setStateData(products: List<Product>?) = with(binding) {
+        recyclerViewProducts.isVisible = products != null
+        adapter.setProducts(products ?: emptyList())
+    }
+
+    private fun setStateError(error: String?) = with(binding) {
+        constraintLayoutError.isVisible = error != null
+        textViewError.text = error
+    }
+}
+
+sealed class ScreenState {
+    data class DataLoaded(val products: List<Product>) : ScreenState()
+    object Loading : ScreenState()
+    data class Error(val error: String) : ScreenState()
 }
